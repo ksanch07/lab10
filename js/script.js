9aade854f705a77904964c060fd59e46df15cc48
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-03-26 18:40:22 -0700

    still getting eorr, codium no help

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ const toggle = (e) => {
     // get the currently selected h2 element
     let h2 = e.currentTarget;
     // get the div for the currently selected h2 element
-    let div = h2.nextElementSibling;
+    let div = h2.nextElementSibling
 
     // handle removal of all calsses from h2 and div elemetns
     // loop through all h2 elements
     for (h2Element of h2Elements) {
-        if (h2Element !=== e.currentTarget) {
+        if (h2Element !== e.currentTarget) {
             //remove class from h2
             h2Element.removeAttribute('class');
             h2Element.nextElementSibling.removeAttribute('class');
@@ -22,23 +22,18 @@ const toggle = (e) => {
     }
 
    // toggle plus and minus icons
-   if h2.hasAttribute('class') {
+   if (h2.hasAttribute('class')) {
        h2.removeAttribute('class');
    } else {
        h2.setAttribute('class', 'minus');
    }  
     // toggle open/close panel for selected h2 element
-    if div.hasAttribute('class') {
+    if (div.hasAttribute('class')) {
         div.removeAttribute('class');
     } else {
         dv.setAttribute('class', 'open');
     }  
    
-    // // toggle the active class on the currently selected h2 element
-    // h2.classList.toggle('active');
-    // // toggle the active class on the div for the currently selected h2 element
-    // div.classList.toggle('active');
-    
 }
 
 //l loop through collection of h2 elements and attach toggle event handler to each
